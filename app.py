bed239db2d63a284debb55020c76c66ca4816613
Grant <gth2103@columbia.edu>
2019-10-12 02:27:45 -0400

    added rooms, chaned user group to course members

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import flask
+import time
 from app import app
-from flask_socketio import SocketIO, send
+from flask import request
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 from flask_login import current_user
+from flask_socketio import ConnectionRefusedError
 
 # from flask_login import
 
@@ -15,17 +18,49 @@ from flask_login import current_user
 
 socketio = SocketIO(app)
 
-
 @socketio.on('message')
-def handleMessage(msg):  
-    print("message:")
-    print(msg) 
-    print(current_user)
+def handleMessage(data): 
+	print(request.sid)
+	print("message:")
+	print(data)
+	print(current_user)
 
-    #This is where I should save it to the DB WITH THE USER NAME
-    send(msg, broadcast = True)
+	#This is where I should save it to the DB WITH THE USER NAME
 
-#print("mesg:",msg)
+	#send(msg, broadcast = True)
+	send({'msg': data['msg'], 'user': data['user'], 'time': time.strftime('%b %d %I:%M%p', time.localtime()), 'room': data['room']}, room = data['room'])
+
+@socketio.on('join')
+def join(data):
+
+	join_room(data['room'])
+	send({'msg': data['user'] + " has joined the room.", 'user': data['room'], 'time': time.strftime('%b %d %I:%M%p', time.localtime())}, room = data['room'])
+
+@socketio.on('leave')
+def leave(data):
+
+	leave_room(data['room'])
+	send({'msg': data['user'] + " has left the room.", 'user': data['room'], 'time': time.strftime('%b %d %I:%M%p', time.localtime())}, room = data['room'])
+#@socketio.on('image')
+#def handleImage(img):
+#	print("image: ")
+#	print(img)
+#	file = open(img, 'rb')
+#	image_bin = file.read()
+#	emit('image', { image_bin: image_bin}, broadcast = True);
+
+@socketio.on('disconnect')
+def test_disconnect():
+	print('Client disconnected')
+
+@socketio.on('connect')
+def connect_handler():
+    if current_user.is_authenticated:
+        emit('my response',
+             {'message': '{0} has joined'.format(current_user.username)})
+    else:
+        return False  # not allowed here
+
  
 if __name__ == "__main__":
     #app.run(debug=True)
